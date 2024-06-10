8cc50ce5bc875ae8c534920f3791b09dab41418c
kevin kamla <kvnkamla4@gmail.com>
2024-06-10 18:49:04 +0100

    Get Started PYTHON

---
+++ week2/Day1/day1.py
@@ -0,0 +1,40 @@
+
+# print("Addition ", 5 + 10)
+# print("Soustraction ", 20 - 5)
+# print("Product ", 15 * 2)
+# print("Division ", 30 / 3)
+# print("Modulo ", 30 % 7) 
+
+# x = 10
+# print(type(x))
+
+# y = 10.8
+# print(type(y))
+
+# z = False
+# print(type(z))
+
+# a = "Yo bro"
+# print(type(a))
+
+# print("hello bro, how are you ? \n ergrgergerg ergregergregerg ")
+# print("hello bro, how are you ?".capitalize())
+# print("hello bro, how are you ?".upper())
+#  print("HELLO BRO, HOW ARE YOU ?".lower())
+
+# my_age = 15
+# print(my_age, type(my_age))
+
+# my_age_converted_to_string = str(my_age)
+# print(my_age_converted_to_string, type(my_age_converted_to_string))
+
+# print("ma" + "ma")
+# print("ma" * 2)
+# print("ma" + 2)
+# print("ma" +" "  +  "ma")
+
+x = 10
+y = 10
+
+print(True and False)
+
