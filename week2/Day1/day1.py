2ebdfe92b793ff4b79805839135267a21815a9fa
kevin kamla <kvnkamla4@gmail.com>
2024-06-12 10:10:05 +0100

    Day1 finish and exercice

--- week2/Day1/day1.py
+++ week2/Day1/day1.py
@@ -33,8 +33,49 @@
 # print("ma" + 2)
 # print("ma" +" "  +  "ma")
 
-x = 10
-y = 10
+# x = 10
+# y = 10
 
-print(True and False)
+# print(True and False)
 
+# tr = "fsdfsdfsdfsddfhdfhdfhdfhdfhdfhdfhdfhdfhdfhdfhdf\
+#     fdfhdfhdfhdfhdfhdfhdfhfdhfdhfdhfdhfdh fhdfdf"
+
+# number = input("Please enter a number : \n")
+
+# my_name = "Kevin"
+# my_age =  50
+
+# print("\nMon nom c'est "+ my_name +" et j'ai " , my_age , " ans" )
+# print(f"\nMon nom c'est {my_name}  et j'ai  {my_age}  ans" )
+
+# number = int(input('Multiply me by three: '))
+# result = number * 3
+# print(result)
+
+# a = 33
+# b = 200
+
+# if a > b:
+#     print("a es plus petit que b")
+# else:
+#     print("a es plus grand que b")
+
+note = int(input("\nEntre ta moyenne : "))
+
+# if note < 8 :
+#     print("\nTu es trop faible !!!")
+# elif note >= 8 and note < 12:
+#     print("\nTu es faible !!!")
+# elif note >= 12 and note < 15:
+#     print("\nTu es Moyen !!!")
+# elif 15 <= note <= 20:
+#     print("\nTu es Excellent !!!")
+# else:
+#     print("\n Entre une valeur valide")
+    
+    
+if  note != 10 and note == 20:
+    print("=========== 1 ==========")
+else:
+    print("=========== 2 ==========")
